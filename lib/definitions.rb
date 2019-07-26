2c1026147b030c07af37a4b3c96f49692f7f160f
jirwincline <jakobinwins@gmail.com>
2019-07-26 15:45:54 -0700

    start building out the definitions rb file

--- lib/definitions.rb
+++ lib/definitions.rb
@@ -1,3 +1,25 @@
+require("pry")
+require("word")
+
 class Definition
+  attr_reader :id
+  attr_accessor :text, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(text, word_id, id=nil)
+    @text = text
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all()
+    @@definitions.values()
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
 
 end
