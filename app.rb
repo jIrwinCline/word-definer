3a5ea5719124fe276e2472a78752f36e20fba59c
jirwincline <jakobinwins@gmail.com>
2019-07-26 09:38:48 -0700

    add routes to app rb

--- app.rb
+++ app.rb
@@ -3,3 +3,30 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/words.rb')
 require('./lib/definition')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get ('/words/new') do
+  erb(:new_word)
+end
+
+get ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+post ('/words') do
+  word = params[:word]
+  word = Word.new(word)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
